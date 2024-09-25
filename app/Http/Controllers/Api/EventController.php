874b1bd31c80b114c6d20d14c8e689a1fbaafbcb
Wasim Hyder <wasim@creativefaze.com>
2024-09-25 20:12:23 +0500

    Created event via api request

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,47 +3,54 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
-class EventController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+class EventController extends Controller {
+  /**
+   * Display a listing of the resource.
+   */
+  public function index() {
+    return Event::all();
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request) {
+    $event = Event::create([
+      ...$request->validate([
+        'name' => 'required|string|max:255',
+        'description' => 'nullable|string',
+        'start_time' => 'required|date',
+        'end_time' => 'required|date|after_or_equal:start_time'
+      ]),
+      'user_id' => 1
+    ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+    return $event;
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(Event $event) {
+    return $event;
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, string $id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(string $id)
+  {
+    //
+  }
 }
--- app/Models/Event.php
+++ app/Models/Event.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Event extends Model {
   use HasFactory;
 
+  protected $fillable = ['name', 'description', 'start_time', 'end_time', 'user_id'];
   public function user() : BelongsTo {
     return $this->belongsTo(User::class);
   }
