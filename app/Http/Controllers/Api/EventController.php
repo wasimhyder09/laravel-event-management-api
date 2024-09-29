d6b25c6d280c8a8e1568f515315ffb3400c8fe7d
Wasim Hyder <wasim@creativefaze.com>
2024-09-29 10:00:47 +0500

    Implemented query based relationships

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -12,7 +12,28 @@ class EventController extends Controller {
    * Display a listing of the resource.
    */
   public function index() {
-    return EventResource::collection(Event::with('user', 'attendees')->paginate());
+    $query = Event::query();
+    $relations = ['user', 'attendees', 'attendees.user'];
+
+    foreach ($relations as $relation) {
+      $query->when(
+        $this->shouldIncludeRelations($relation),
+        fn ($q) => $q->with($relation)
+      );
+    }
+
+    return EventResource::collection(
+      $query->latest()->paginate()
+    );
+  }
+
+  protected function shouldIncludeRelations(string $relation): bool {
+    $include = request()->query('include');
+    if(!$include) {
+      return false;
+    }
+    $relations = array_map('trim', explode(',', $include));
+    return in_array($relation, $relations);
   }
 
   /**
