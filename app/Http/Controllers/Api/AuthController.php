4057825fa5ee4a8779a822ad889e7102d25b06f4
Wasim Hyder <wasim@creativefaze.com>
2024-09-29 21:57:36 +0500

    Implemented restriction on events controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
-
 class AuthController extends Controller {
   public function login(Request $request) {
     $request->validate([
--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -13,6 +13,10 @@ class EventController extends Controller {
   use CanLoadRelationships;
 
   private array $relations = ['user', 'attendees', 'attendees.user'];
+
+  public function __construct() {
+    $this->middleware('auth:sanctum', ['except' => ['index', 'show']]);
+  }
   public function index() {
 
     $query = $this->loadRelationships(Event::query());
@@ -30,7 +34,7 @@ class EventController extends Controller {
         'start_time' => 'required|date',
         'end_time' => 'required|date|after_or_equal:start_time'
       ]),
-      'user_id' => 1
+      'user_id' => $request->user()->id
     ]);
 
     return new EventResource($this->loadRelationships($event));
