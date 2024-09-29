1278867dca8dc7e3782f270ee14bcdc3573012a5
Wasim Hyder <wasim@creativefaze.com>
2024-09-29 11:59:02 +0500

    Created trait to load opational paramaeters for all functions

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -4,41 +4,24 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\EventResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller {
-  /**
-   * Display a listing of the resource.
-   */
-  public function index() {
-    $query = Event::query();
-    $relations = ['user', 'attendees', 'attendees.user'];
 
-    foreach ($relations as $relation) {
-      $query->when(
-        $this->shouldIncludeRelations($relation),
-        fn ($q) => $q->with($relation)
-      );
-    }
+  use CanLoadRelationships;
+
+  private array $relations = ['user', 'attendees', 'attendees.user'];
+  public function index() {
+
+    $query = $this->loadRelationships(Event::query());
 
     return EventResource::collection(
       $query->latest()->paginate()
     );
   }
 
-  protected function shouldIncludeRelations(string $relation): bool {
-    $include = request()->query('include');
-    if(!$include) {
-      return false;
-    }
-    $relations = array_map('trim', explode(',', $include));
-    return in_array($relation, $relations);
-  }
-
-  /**
-   * Store a newly created resource in storage.
-   */
   public function store(Request $request) {
     $event = Event::create([
       ...$request->validate([
@@ -50,20 +33,13 @@ class EventController extends Controller {
       'user_id' => 1
     ]);
 
-    return new EventResource($event);
+    return new EventResource($this->loadRelationships($event));
   }
 
-  /**
-   * Display the specified resource.
-   */
   public function show(Event $event) {
-    $event->load('user', 'attendees');
-    return new EventResource($event);
+    return new EventResource($this->loadRelationships($event));
   }
 
-  /**
-   * Update the specified resource in storage.
-   */
   public function update(Request $request, Event $event) {
     $event->update(
       $request->validate([
@@ -73,12 +49,9 @@ class EventController extends Controller {
         'end_time' => 'sometimes|date|after_or_equal:start_time'
       ])
     );
-    return new EventResource($event);
+    return new EventResource($this->loadRelationships($event));
   }
 
-  /**
-   * Remove the specified resource from storage.
-   */
   public function destroy(Event $event) {
     $event->delete();
     return response()->json([
