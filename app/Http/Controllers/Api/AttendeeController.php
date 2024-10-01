1daa834fb71778c5f4e4dabb9d98dc676ed41f66
Wasim Hyder <wasimhyder09@gmail.com>
2024-10-01 19:39:15 +0500

    Made attendee id dynamic based on request

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -25,7 +25,7 @@ class AttendeeController extends Controller {
    */
   public function store(Request $request, Event $event) {
     $attendee = $event->attendees()->create([
-      'user_id' => 1
+      'user_id' => $request->user()->id
     ]);
 
     return new AttendeeResource($attendee);
